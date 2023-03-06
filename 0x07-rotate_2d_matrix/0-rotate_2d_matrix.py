7ec8664e3dd5a261dd06125ab138a9ec772e56f5
selam123 <beletelakachew@gmail.com>
2023-03-06 07:21:24 +0000

    jhglkhd

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env fish
-python3 -m doctest -v (basename (status -f))
-exit
-"""
->>> rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
->>> test_cases = [
-...     [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-...     [[1, 2, 3, 15], [4, 5, 6, 13]],
-... ]
->>> for matrix in test_cases:
-...     rotate_2d_matrix(matrix)
-...     print(matrix)
-[[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-[[4, 1], [5, 2], [6, 3], [13, 15]]
-"""
+#!/usr/bin/python3
+''' rotates a 2D matrix '''
+
+
+def rotate_2d_matrix(matrix):
+    '''
+        rotates 2d matrix 90 degrees clockwise
+        rotation happens in place
+        args:
+            matrix
+    '''
+    rows = len(matrix)
+    columns = rows - 1
+
+    for i in range(0, int(rows / 2)):
+        for j in range(i, columns - i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[columns - j][i]
+            matrix[columns - j][i] = matrix[columns - i][columns - j]
+            matrix[columns - i][columns - j] = matrix[j][columns - i]
+            matrix[j][columns - i] = temp
